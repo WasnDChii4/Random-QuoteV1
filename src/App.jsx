f56e87d283987ba11d2e3770b5b9dc3b915d44b6
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-10-19 15:00:17 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import RandomQuote from "./components/RandomQuote";
+
+export default function App() {
+  return (
+    <div className="bg-gradient-to-br from-[#3A3A52] to-[#1E1E2F] min-h-screen">
+      <RandomQuote />
+    </div>
+  )
+}
