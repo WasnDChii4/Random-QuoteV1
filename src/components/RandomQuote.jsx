de22c577f1ed56c22e2a1ce52594365142aba14c
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-10-20 10:48:25 +0700

    Update code

--- src/components/RandomQuote.jsx
+++ src/components/RandomQuote.jsx
@@ -1,12 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function RandomQuote() {
-  const [quote, setQuote] = useState('');
-  const [author, setAuthor] = useState('');
-  const [loading, setLoading] = useState(true);
-
-  
-
   return (
     <div className="bg-[#2A2A40]/70 text-white place-self-center w-11/12 max-w-md flex flex-col p-3 md:p-7 rounded-xl backdrop-blur-xl border border-white/15 shadow-md shadow-black">
       <p className="text-3xl text-center">Halow</p>
