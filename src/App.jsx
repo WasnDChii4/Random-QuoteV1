83ffe9e26385d3ddbd7a7f82663b35297f21fbd3
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-10-20 10:10:08 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import RandomQuote from "./components/RandomQuote";
 
 export default function App() {
   return (
-    <div className="bg-gradient-to-br from-[#3A3A52] to-[#1E1E2F] min-h-screen">
+    <div className="bg-gradient-to-br from-[#3A3A52] to-[#1E1E2F] min-h-screen grid">
       <RandomQuote />
     </div>
   )
--- src/components/RandomQuote.jsx
+++ src/components/RandomQuote.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function RandomQuote() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  
+
   return (
-    <div>
-      
+    <div className="bg-[#2A2A40]/70 text-white place-self-center w-11/12 max-w-md flex flex-col p-3 md:p-7 rounded-xl backdrop-blur-xl border border-white/15 shadow-md shadow-black">
+      <p className="text-3xl text-center">Halow</p>
     </div>
   )
 }
